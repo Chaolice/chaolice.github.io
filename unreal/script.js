76c3587effe32d91554e4102385245a9b08f3a78
Mélanie <73541645+Chaolice@users.noreply.github.com>
2023-01-12 11:15:06 -0500

    unreal

---
+++ unreal/script.js
@@ -0,0 +1,20 @@
+
+
+// window.addEventListener("load", function (event) {
+//     console.log("yolo")
+
+//     let reponses = document.getElementsByTagName("tw-expression")
+//     let topAnswer = reponses.item(0)
+//     let bottomAnswer = reponses.item(1)
+   
+
+//     function clickTop() {
+//         console.log("click top")
+//         topAnswer.firstChild.click();
+//     }
+
+//     function clickBottom(){
+//         console.log("click bottom")
+//         bottomAnswer.firstChild.click();
+//     }
+// });
